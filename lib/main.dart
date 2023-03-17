00c3292087dd2c312397746899fa9d3edf614dc6
Ivan Ovcharov <xxovcharovxx@gmail.com>
2023-03-18 00:22:49 +0300

    Bug fixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:calculator_app/expression.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(CalculatorApp());
@@ -20,7 +21,10 @@ class _CalculatorApp extends State<CalculatorApp> {
 
   @override
   Widget build(BuildContext context) {
-    print('build ' + expression.getExpression());
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -28,70 +32,81 @@ class _CalculatorApp extends State<CalculatorApp> {
         backgroundColor: Colors.black54,
         appBar: _buildAppBar(),
         body: Container(
-          margin: EdgeInsets.symmetric(horizontal: 10),
+          margin: EdgeInsets.symmetric(horizontal: 15),
           //padding: EdgeInsets.symmetric(horizontal: 10),
-          alignment: Alignment.topCenter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                height: 50,
-              ),
-              Text(
-                expression.getExpression(),
-                textAlign: TextAlign.right,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 50,
+                  child: Text(
+                    'Scroll to see full expression',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildButton('C'),
-                  _buildButton('('),
-                  _buildButton(')'),
-                  _buildButton('/'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildButton('7'),
-                  _buildButton('8'),
-                  _buildButton('9'),
-                  _buildButton('*'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildButton('4'),
-                  _buildButton('5'),
-                  _buildButton('6'),
-                  _buildButton('-'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildButton('1'),
-                  _buildButton('2'),
-                  _buildButton('3'),
-                  _buildButton('+'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildButton('.'),
-                  _buildButton('0'),
-                  _buildButton('<='),
-                  _buildButton('='),
-                ],
-              )
-            ],
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Text(
+                    expression.getExpression(),
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildButton('C'),
+                    _buildButton('('),
+                    _buildButton(')'),
+                    _buildButton('/'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildButton('7'),
+                    _buildButton('8'),
+                    _buildButton('9'),
+                    _buildButton('*'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildButton('4'),
+                    _buildButton('5'),
+                    _buildButton('6'),
+                    _buildButton('-'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildButton('1'),
+                    _buildButton('2'),
+                    _buildButton('3'),
+                    _buildButton('+'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildButton('.'),
+                    _buildButton('0'),
+                    _buildButton('<='),
+                    _buildButton('='),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -119,7 +134,8 @@ class _CalculatorApp extends State<CalculatorApp> {
       style: ElevatedButton.styleFrom(
         backgroundColor: buttonColor,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(200)),
-        fixedSize: Size(85, 85),
+        fixedSize: Size(73, 73),
+        minimumSize: Size(60, 60),
       ),
       child: Text(
         data,
